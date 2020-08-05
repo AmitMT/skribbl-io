8090c62bd00ea6d4aa0d335644134521fc3c0cb7
AmitMT <amit.mathov@gmail.com>
2020-08-05 17:53:40 +0300

    alert

--- public/pages/game/sketch.js
+++ public/pages/game/sketch.js
@@ -1,5 +1,6 @@
 function setup() {
 	socket = io.connect(location.protocol + '//' + location.host + location.pathname);
+	alert(location.protocol + '//' + location.host + location.pathname);
 	alert(window.location.href);
 	socket.on('id', (data) => {
 		id = data;
